33cf152716c79c3bf3e4a8420ecb69190ffc4d4d
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-09-18 22:56:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ from os import environ
 class Config(object):
     API_ID = int(environ.get("API_ID", 16743442))
     API_HASH = environ.get("API_HASH", "12bbd720f4097ba7713c5e40a11dfd2a")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "6206599982:AAGMtdPmgw7wRF5EsdREiJT3Qb9Yy8ALIVE")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "6206599982:AAGqJ84tpTzhdKYzNRMp2kPdcpN0_1zz5K4")
     MONGO_STR = environ.get("MONGO_STR", "mongodb+srv://jarvis:op@cluster0.7tisvwv.mongodb.net/?retryWrites=true&w=majority")
 
